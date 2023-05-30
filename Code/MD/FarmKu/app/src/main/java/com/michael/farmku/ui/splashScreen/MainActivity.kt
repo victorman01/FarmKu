2023fecf9b4adc7102deebebcdf141e30eec16c8
Michael-Tio <mtio161@gmail.com>
2023-05-30 19:20:41 +0700

    Ngulang Login Google sudah selesai 90% tapi masih error

--- Code/MD/FarmKu/app/src/main/java/com/michael/farmku/ui/splashScreen/MainActivity.kt
+++ Code/MD/FarmKu/app/src/main/java/com/michael/farmku/ui/splashScreen/MainActivity.kt
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.michael.farmku.R
 import com.michael.farmku.databinding.ActivityMainBinding
 import com.michael.farmku.ui.banner.Banner1Activity
+import com.michael.farmku.ui.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +30,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, Banner1Activity::class.java)
-            startActivity(intent)
-            finish()
-        }, 2000L)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null){
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 2000L)
+        }
+        else{
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, Banner1Activity::class.java)
+                startActivity(intent)
+                finish()
+            }, 2000L)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 }
