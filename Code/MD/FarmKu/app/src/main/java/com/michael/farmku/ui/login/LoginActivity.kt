68ded3a84474029065b845c537a90b1b794d49be
Michael-Tio <mtio161@gmail.com>
2023-05-30 18:05:34 +0700

    Error

--- Code/MD/FarmKu/app/src/main/java/com/michael/farmku/ui/login/LoginActivity.kt
+++ Code/MD/FarmKu/app/src/main/java/com/michael/farmku/ui/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.michael.farmku.ui.login
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import android.app.Activity
 import android.content.Intent
+=======
+>>>>>>> parent of 3ac9490 (Login, Forgot Password)
 =======
 >>>>>>> parent of 3ac9490 (Login, Forgot Password)
 import androidx.appcompat.app.AppCompatActivity
@@ -21,9 +24,12 @@ import com.google.firebase.ktx.Firebase
 import com.michael.farmku.R
 import com.michael.farmku.databinding.ActivityLoginBinding
 <<<<<<< HEAD
+<<<<<<< HEAD
 import com.michael.farmku.ui.home.HomeActivity
 import com.michael.farmku.ui.forgotPassword.ForgotPasswordActivity
 import com.michael.farmku.ui.register.RegisterActivity
+=======
+>>>>>>> parent of 3ac9490 (Login, Forgot Password)
 =======
 >>>>>>> parent of 3ac9490 (Login, Forgot Password)
 
@@ -40,6 +46,7 @@ class LoginActivity : AppCompatActivity() {
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+<<<<<<< HEAD
 <<<<<<< HEAD
 
         val gso = GoogleSignInOptions
@@ -114,6 +121,8 @@ class LoginActivity : AppCompatActivity() {
     }
     companion object {
         private const val TAG = "LoginActivity"
+=======
+>>>>>>> parent of 3ac9490 (Login, Forgot Password)
 =======
 >>>>>>> parent of 3ac9490 (Login, Forgot Password)
     }
