c3329724b1f7b2e89c2668f09c259814f8bf5729
Michael-Tio <mtio161@gmail.com>
2023-06-02 02:56:12 +0700

    Register 90% Finished UI

--- Code/MD/app/src/main/java/com/farmkuindonesia/farmku/ui/register/RegisterFillDataActivity.kt
+++ Code/MD/app/src/main/java/com/farmkuindonesia/farmku/ui/register/RegisterFillDataActivity.kt
@@ -2,15 +2,16 @@ package com.farmkuindonesia.farmku.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.farmkuindonesia.farmku.databinding.ActivityRegisterFillDataBinding
 import com.farmkuindonesia.farmku.databinding.ActivityRegisterOtpBinding
 
-class RegisterOtpActivity : AppCompatActivity() {
-    private var _binding: ActivityRegisterOtpBinding? = null
+class RegisterFillDataActivity : AppCompatActivity() {
+    private var _binding: ActivityRegisterFillDataBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityRegisterOtpBinding.inflate(layoutInflater)
+        _binding = ActivityRegisterFillDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
     }
