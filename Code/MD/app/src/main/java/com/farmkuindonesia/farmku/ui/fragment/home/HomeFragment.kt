0a4132fe30be557bdaf704ddcd77302f8b5b6664
Victor Manuel Soesilo <79180895+victorman01@users.noreply.github.com>
2023-06-08 21:46:41 +0700

    pilih camera atau gambar plus permission udah bisa, cmn belum 100% masih deprecated, masih cari cara lain

--- Code/MD/app/src/main/java/com/farmkuindonesia/farmku/ui/fragment/home/HomeFragment.kt
+++ Code/MD/app/src/main/java/com/farmkuindonesia/farmku/ui/fragment/home/HomeFragment.kt
@@ -1,12 +1,20 @@
 package com.farmkuindonesia.farmku.ui.fragment.home
 
-import android.content.Context
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.farmkuindonesia.farmku.R
 import com.farmkuindonesia.farmku.databinding.FragmentHomeBinding
@@ -14,16 +22,25 @@ import com.farmkuindonesia.farmku.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private var currentPermissionRequestCode: Int = 0
+    private val CAMERA_PERMISSION_REQUEST_CODE = 1001
+    private val GALLERY_PERMISSION_REQUEST_CODE = 1002
+    private val CAMERA_REQUEST_CODE = 2001
+    private val GALLERY_REQUEST_CODE = 2002
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         val adapter = ListNewsAdapter(getDummyNewsList())
-        binding.rvNews.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvNews.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         binding.rvNews.adapter = adapter
 
+        binding.btnDeteksiPenyakit.setOnClickListener {
+            showOptionsDialog()
+        }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,13 +51,134 @@ class HomeFragment : Fragment() {
 
     private fun getDummyNewsList(): ArrayList<Dummy> {
         val dummyList = ArrayList<Dummy>()
-        dummyList.add(Dummy("Merangkak Naik, Harga TBS Sawit di Babel Naik Rp 100 dari Rp 1.300 per Kg menjadi Rp 1.400 per Kg", "June 1, 2023", R.drawable.home_banner))
-        dummyList.add(Dummy("Distan Mataram mengoptimalkan pengolahan tanah tingkatkan produksi padi", "June 2, 2023", R.drawable.home_banner))
-        dummyList.add(Dummy("Gagal Tanam Padi, Petani Desa Rahayu Minta PHE TEJ Beri Ganti Rugi", "June 3, 2023", R.drawable.home_banner))
+        dummyList.add(
+            Dummy(
+                "Merangkak Naik, Harga TBS Sawit di Babel Naik Rp 100 dari Rp 1.300 per Kg menjadi Rp 1.400 per Kg",
+                "June 1, 2023",
+                R.drawable.home_banner
+            )
+        )
+        dummyList.add(
+            Dummy(
+                "Distan Mataram mengoptimalkan pengolahan tanah tingkatkan produksi padi",
+                "June 2, 2023",
+                R.drawable.home_banner
+            )
+        )
+        dummyList.add(
+            Dummy(
+                "Gagal Tanam Padi, Petani Desa Rahayu Minta PHE TEJ Beri Ganti Rugi",
+                "June 3, 2023",
+                R.drawable.home_banner
+            )
+        )
         return dummyList
     }
-    companion object {
-        @JvmStatic
-        fun newInstance() = HomeFragment().apply {}
+
+    private fun showOptionsDialog() {
+        val options = arrayOf("Ambil Gambar dengan Kamera", "Pilih Gambar dari Galeri")
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Pilih Sumber Gambar")
+            .setItems(options) { dialog: DialogInterface, which: Int ->
+                when (which) {
+                    0 -> checkCameraPermission()
+                    1 -> checkGalleryPermission()
+                }
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+    private fun checkCameraPermission() {
+        currentPermissionRequestCode = CAMERA_PERMISSION_REQUEST_CODE
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            openCamera()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun checkGalleryPermission() {
+        currentPermissionRequestCode = GALLERY_PERMISSION_REQUEST_CODE
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            openGallery()
+        } else {
+            requestGalleryPermission()
+        }
+    }
+
+    private fun requestGalleryPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            GALLERY_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                }
+            }
+            GALLERY_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_REQUEST_CODE -> {
+                    val imageFromCamera = data?.extras?.get("data") as Bitmap
+                    // Lakukan sesuatu dengan gambar dari kamera
+                }
+                GALLERY_REQUEST_CODE -> {
+                    val uri = data?.data
+                    // Lakukan sesuatu dengan gambar dari galeri
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
