d8fd537e0221b0172b1ec6819c98c25874bf1d63
Michael-Tio <mtio161@gmail.com>
2023-06-16 16:05:12 +0700

    design fixing

--- Code/MD/app/src/main/java/com/farmkuindonesia/farmku/ui/fragment/listland/measurement/MeasurementActivity.kt
+++ Code/MD/app/src/main/java/com/farmkuindonesia/farmku/ui/fragment/listland/measurement/MeasurementActivity.kt
@@ -3,13 +3,17 @@ package com.farmkuindonesia.farmku.ui.fragment.listland.measurement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.farmkuindonesia.farmku.R
+import com.farmkuindonesia.farmku.databinding.ActivityAddLandBinding
+import com.farmkuindonesia.farmku.databinding.ActivityMeasurementBinding
 
 class MeasurementActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMeasurementBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_measurement)
+        binding = ActivityMeasurementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
